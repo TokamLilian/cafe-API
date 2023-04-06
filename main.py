651e81b7227d44e1d75980540e5802c00d37a10f
TokamLilian <tokamlilian@gmail.com>
2023-04-06 13:04:08 -0400

    Finished with the function to read all information in menu.json

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 #import requests
 
 from message_section import *
@@ -11,7 +12,7 @@ from message_section import *
 
 compte =    'C:\\Users\\tokam\Dropbox\\XPS School Docs\\H23\\IFT 1015\\Exercices Notés\\TP 2\\cafe-api\\files\\comptes.csv'
 commande =  'C:\\Users\\tokam\Dropbox\\XPS School Docs\\H23\\IFT 1015\\Exercices Notés\\TP 2\\cafe-api\\files\\commandes.csv'
-menu =      'C:\\Users\\tokam\Dropbox\\XPS School Docs\\H23\\IFT 1015\\Exercices Notés\\TP 2\\cafe-api\\files\\menu.csv'
+menu =      'C:\\Users\\tokam\Dropbox\\XPS School Docs\\H23\\IFT 1015\\Exercices Notés\\TP 2\\cafe-api\\files\\menu.json'
 
 def chercher(repertoire, informations):
     statut = 'inexisting_user'
@@ -75,4 +76,72 @@ def get_argument():
     except :
         get_inputs()
 
-get_argument()
+#get_argument()
+
+
+def get_items():
+
+    with open(menu) as json_file:
+        the_menu = json.load(json_file)
+
+        for type in the_menu:
+            print('Type', type)                             #les differents type d'items
+            category = the_menu[type]                       #les categories pour chaque type
+            #print('on a', category)
+            for items in category:                          
+                #print(item)
+                sub_category = category[items]              #les sous-categories pour chaque categories de type
+                #print(sub_category)
+
+                for item in sub_category:
+                    #print(item)
+                    try:
+                        item_type = sub_category[item]      #les types d'items sous chaque sous-categories
+                    except:                                 ##fruit et muffin n'ont pas de sous categories, donc on va acceder à "items" directement
+                        item_type = sub_category
+                        
+                    #print(item_type)
+                    for line in item_type:                  #line is "items"
+                        try:
+                            info = item_type[line]
+                        except:                             #certaines categories n'ont pas de sous-categories,
+                                                            #on va acceder aux informations directement
+                            info = item_type
+                        #print(info)                        #le block d'informations d'un type d'item
+                    
+                        for infomation in info:
+                            #print(info[infomation])
+                            print(infomation)
+                        break
+                    
+                    break
+                    
+                    print("")
+                
+
+            print("");print("")
+
+get_items()
+
+
+def get_items_old(the_menu):
+        for item in the_menu:
+            item_name = item.upper()
+            print(item_name)
+
+            list = the_menu[item]
+
+            for type in list:
+                category = list[type]
+
+                #for item in category:
+                    #print(item)
+                    #print(the_menu[list][category][item].get("items"))
+
+                for i in range(len(category)):
+                    item = category[i]
+                    print(item)
+
+
+        #print(items)
+        #print(json.dumps(items,indent=5, sort_keys=True))
