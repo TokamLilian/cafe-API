53904dc4bd15df6c2d72a2a0cc2ac6f52a69937a
TokamLilian <tokamlilian@gmail.com>
2023-04-09 22:20:23 -0400

    Added all get actions and adjusted the post funtion

--- main.py
+++ main.py
@@ -34,7 +34,24 @@ def chercher(repertoire, informations):
     return statut
 
 
-def get_items(key):
+def get_other(name,key):
+#cette fontion parcour les comptes ou commandes pour l'utilisateur
+    #with open(comptes) as accounnt_file:
+        dir = globals()[name]
+        file = open(dir, "r", encoding= 'utf-8')
+        for line in file:
+            try:
+                id = int(key)
+                text = line.split('|')
+                matricule = text[0].strip() 
+
+                if id == int(matricule):
+                    print(line);break
+            except:
+                print(line)
+
+
+def get_menu(key):
 
     def get_id_info(key, sub_category_items):
         for infomation in sub_category_items: 
@@ -51,19 +68,19 @@ def get_items(key):
 
     with open(menu) as json_file:
         the_menu = json.load(json_file)
-
+        way = ""
         if key == 'items' or type(key)== int :
             way = the_menu
             if key == 'items':
                 print('menu') 
             elif key < 1 or key > 40:
-                print('Cet item n\'est pas disponible')
+                print('Mauvais identifiant d\'item')
                 return None
 
         else:
-            try:
+            
                 for category in the_menu:
-
+                    if way != "": break
                     if key == category :
                         way = the_menu[key]
                         break
@@ -80,10 +97,9 @@ def get_items(key):
                                     break
                         except: pass
 
-                way and print(key)
+                if way !="": print(key)
 
-            except:
-                print('Chemin non spécifé'); return None
+                else:print('Chemin non spécifié'); return None
 
         for item_name in way:                                                       #les differents type d'items
             if item_name!='items' and type(key)!=int :print(item_name)
@@ -102,7 +118,7 @@ def get_items(key):
                 try:
                     for category_name in categories:  
                                                  
-                        sub_category = categories[category_name]                    #les sous-categories pour chaque categories de type
+                        sub_category = categories[category_name]                                   #les sous-categories pour chaque categories de type
                         if type(key)!=int:print(category_name)
 
                         for sub_category_name in sub_category:
@@ -122,40 +138,70 @@ def get_items(key):
                                 
                         type(key)!= int and print("")
                 except:
-                    sub_category_items = way["items"]
-                    if type(key)== int:get_id_info(key, sub_category_items)
-                    else:print_key(sub_category_items)
+                        sub_category_items = way["items"]
+                        if type(key)== int:get_id_info(key, sub_category_items)
+                        else:print_key(sub_category_items)
         
             type(key)!= int and print("")
 
 
 def GET(path):
 #cette fonction affiche la liste d'une categorie
-    key_index = path.index('menu') + 1
-    key = path[key_index]
-    get_items(key)
-  
+    last = len(path) - 1
+    key = path[last] 
+
+    name = path[1]
+    try:
+        id = int(key)
+        if name == 'menu':
+            if path[last-1] != 'items': print('Chemin specifié incorrect'); return None     ##pour s'assurer qu'on veut get items/id
+            info = get_menu(id)
+
+            if info['disponible'] == True:
+                disponibilite = 'disponible'
+            else: disponibilite = 'Non disponible'
+
+            print(info['id'], info['nom'], 'Prix:',str(info['prix'])+'$', 'est',disponibilite)
+
+        else:
+            get_other(name, id)
+
+    except:
+            if len(path) > 2 and name != 'menu' or key== '': 
+                print('Chemin specifié incorrect')
+                return None
+            
+            if name != 'menu':
+                get_other(name, key)
+
+            else:    
+                key = path[2]
+                get_menu(key)
+        
 
 def POST(matricule, commande):
 #cette fonction crée une commande
 
     prix_total = 0
     for item in commande.split(" "):
-        item = item.split('x')
-        quantite = int(item[0])
+        try:
+            item = item.split('x')
+            quantite = int(item[0])
 
-        item_id = int(item[1])
-        info = get_items(item_id)
+            item_id = int(item[1])
+            info = get_menu(item_id)
 
-        if info["disponible"] == False : 
-            print('L\'item', item_id, 'n\'est pas disponible')
-        
-        else:
-            unit_price = info["prix"]
-            prix_total += unit_price * quantite
+            if info["disponible"] == False : 
+                print('L\'item', item_id, 'n\'est pas disponible')
+            
+            else:
+                unit_price = info["prix"]
+                prix_total += unit_price * quantite
+        except:
+            print('Mauvaise commande en',"x".join(item))
 
+    if prix_total == 0: print('Commande non postée');return None
     prix_total = round(prix_total, 2)
-
     with open(commandes) as order_file:
         for line in order_file:
             pass
@@ -171,6 +217,7 @@ def POST(matricule, commande):
     
     #orders.write('\n')
     #orders.write(order_line)
+    print("");print('Commande postée avec succès')
     orders.close
 
 
@@ -180,9 +227,6 @@ def PUT(path):
 
 
 def process(instruction, path, matricule):
-    print('La commande', instruction)   ##
-    print('Le chemin', path)        ##
-
     callee = globals()[instruction]
 
     if instruction == 'POST':
@@ -197,7 +241,7 @@ def process(instruction, path, matricule):
 
 
 def take_command(statut):
-    #print(statut[0])   ##
+    
     print("""Saisissez -h ou -help pour voir le message d\'aide pour votre statut.
     Entrer une commande souhaitée, ou              
     Entrer FIN pour terminer l'execution.""")
@@ -210,6 +254,7 @@ def take_command(statut):
             help = 'guides_' + statut[0]
             callee = globals()[help]
             callee()
+            break
 
         if command == 'FIN' or command == 'fin': return None
 
@@ -219,42 +264,50 @@ def take_command(statut):
         for _ in range (len(command)):
             word = command[0]
             command.remove(word)
-            word = word.strip()
+            word = word.strip().lower()
             command.append(word)
 
         instruction = command[0]
-        if ' ' in instruction :break
 
-        else:
-            command.remove(instruction)
-            instruction = instruction.upper()
+        command.remove(instruction)
+        instruction = instruction.upper()
+        if ' ' in instruction or instruction not in ['GET', 'POST', 'PUT']:print('Instruction',instruction,'non valide');break
 
-            if len(command) < 2: break
-            if command[0] != 'api':
-                print('Besoin d\'un chemin origin de l\'API pour continuer') 
-                break
+        if instruction == 'POST':
+            part_command = command[1].split(" ")
+            rem = part_command[0]
+            part_command.remove(rem)
+            part_command = " ".join(part_command)
+            command.remove(command[1])
+            command.append(rem) 
+            command.append(part_command) 
 
-            if command[1] not in ['menu', 'commandes', 'comptes']:
-                print('Le chemin spécifé n\'est pas correct')
-                break
+        if len(command) < 2: break
+        if command[0] != 'api':
+            print('Besoin d\'un chemin origin de l\'API pour continuer') 
+            break
 
-            if instruction == 'POST' and command[1] != 'commandes': 
-                print('Vous ne pouvez que poster des commandes.')
-                break
-            
-            if statut[0] != "admin" and command[1] == 'comptes':
-                print('Vous n\'avez pas le droit d\'acceder aux', command[1])
-                break
-            
-            if statut[0] == "public" and (instruction == 'GET' and command[1] == 'commandes' or instruction == 'PUT'):
-                print('Vous n\'avez pas droit à cette fonctionalité')
-                break
-            
-            if instruction == 'PUT' and command[1] == 'commandes': 
-                print('Impossible de faire cette action.')
-                break
+        if command[1] not in ['menu', 'commandes', 'comptes']:
+            print('Le chemin spécifé n\'est pas correct')
+            break
 
-            process(instruction, command, statut[2])
+        if instruction == 'POST' and command[1] != 'commandes': 
+            print('Vous ne pouvez que poster des commandes.')
+            break
+        
+        if statut[0] != "admin" and command[1] == 'comptes':
+            print('Vous n\'avez pas le droit d\'acceder aux', command[1])
+            break
+        
+        if statut[0] == "public" and (instruction == 'GET' and command[1] == 'commandes' or instruction == 'PUT'):
+            print('Vous n\'avez pas droit à cette fonctionalité')
+            break
+        
+        if instruction == 'PUT' and command[1] == 'commandes': 
+            print('Impossible de faire cette action.')
+            break
+
+        process(instruction, command, statut[2])
 
     take_command(statut)
 
@@ -268,7 +321,7 @@ def init(matricule, mot_passe):
     elif statut == 'inexisting_user': print('Utilisateur non touvé')
 
     else:
-        print('Vous êtes connecté')
+        print('Vous êtes connecté');print("")
         
         if statut[1] == '1': 
             take_command(statut)
@@ -294,4 +347,4 @@ def get_arguments():
 
     else: get_inputs()
 
-get_arguments()
+#get_arguments()
